79583d0cc710483f6d259d4dfaac7e0f078f92e9
Efren Lim <efren@favoritemedium.com>
2017-12-06 20:58:59 +0800

    fix error export

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,18 @@
 'use strict';
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Toggle = undefined;
+
 var _Toggle = require('./components/Toggle');
 
 var _Toggle2 = _interopRequireDefault(_Toggle);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-module.exports = {
-  Toggle: _Toggle2.default
-};
+// module.exports = {
+//   Toggle,
+// };
+
+exports.Toggle = _Toggle2.default;
